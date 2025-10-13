e71d91705541fd0251568838d4e389d74d07e0af
Roberto Rodriguez <9653181+Cyb3rWard0g@users.noreply.github.com>
2025-10-13 14:03:10 -0400

    Added llm_activity and agent_activity workflow decorators
    Signed-off-by: Roberto Rodriguez <9653181+Cyb3rWard0g@users.noreply.github.com>

--- dapr_agents/workflow/decorators/__init__.py
+++ dapr_agents/workflow/decorators/__init__.py
@@ -1,5 +1,6 @@
 from .core import task, workflow
 from .fastapi import route
 from .messaging import message_router
+from .activities import llm_activity, agent_activity
 
-__all__ = ["workflow", "task", "route", "message_router"]
+__all__ = ["workflow", "task", "route", "message_router", "llm_activity", "agent_activity"]
