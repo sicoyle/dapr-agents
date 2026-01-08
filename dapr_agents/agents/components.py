f44e80bb33d8cf7c3ce18fbc2ab5130fbd0dc7d5
Casper Nielsen <casper@diagrid.io>
2026-01-08 15:21:07 +0100

    fix(bug): don't set appid from within agentComponents. It's always passed through the payload
    Signed-off-by: Casper Nielsen <casper@diagrid.io>

--- dapr_agents/agents/components.py
+++ dapr_agents/agents/components.py
@@ -481,7 +481,6 @@ class AgentComponents:
             payload["agent"] = {}
 
         payload["agent"]["type"] = type(self).__name__
-        payload["agent"]["appid"] = self.appid
         payload.setdefault("registered_at", datetime.now(timezone.utc).isoformat())
 
         if self._pubsub is not None:
